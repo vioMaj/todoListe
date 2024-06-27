eeeaea7c79358ee8731b8c4c812429188e9c18ab
vioMaj <152890452+vioMaj@users.noreply.github.com>
2024-06-27 17:07:15 +0200

    JS kommentiere

--- todoListe/main.js
+++ todoListe/main.js
@@ -1,9 +1,15 @@
+// Funktion zum Durchsuchen von Einträgen basierend auf dem Suchwort im Suchfeld
 function searchResultat() {
+    // Das Suchwort wird aus dem Suchfeld geholt und in Kleinbuchstaben umgewandelt
     const suchwort = document.getElementById('searchbar').value.toLowerCase();
+    // Alle Elemente mit der Klasse 'entry' werden selektiert
     const eintraege = document.querySelectorAll('.entry');
 
+    // Schleife über alle Einträge
     eintraege.forEach(entry => {
+        // Der Titel jedes Eintrags wird ausgelesen und in Kleinbuchstaben umgewandelt
         const title = entry.querySelector('.boxtitel').textContent.toLowerCase();
+        // Wenn der Titel das Suchwort enthält, wird der Eintrag angezeigt, andernfalls versteckt
         if (title.includes(suchwort)) {
             entry.style.display = 'block';
         } else {
@@ -12,27 +18,38 @@ function searchResultat() {
     });
 }
 
+// Funktion zum Öffnen des Formulars zum Hinzufügen eines neuen Eintrags
 function openHinzufuegen() {
+    // Kein Eintrag wird bearbeitet, es wird ein neuer Eintrag hinzugefügt, darum null
     zuBearbeitenderEintrag = null;
+    // Formular-Titel wird angepasst
     document.getElementById('formTitel').textContent = "Eintrag hinzufügen";
-    document.getElementById('submitButton').textContent = "Hinzufügen";
+    // Formular wird sichtbar gemacht
     document.getElementById('hinzufuegenPop').style.display = 'block';
 }
 
+// Funktion zum Schliessen des Formulars zum Hinzufügen/Bearbeiten eines Eintrags
 function closeHinzufuegen() {
+    // Formular wird unsichtbar gemacht
     document.getElementById('hinzufuegenPop').style.display = 'none';
+    // Formular wird zurückgesetzt
     document.getElementById('formHinzufuegen').reset();
 }
 
+// Funktion zum Löschen eines Eintrags
 function deleteEintrag(button) {
+    // Bestätigungsdialog anzeigen
     let deleteBox = confirm("Sind Sie sicher, dass Sie den ausgewählten Eintrag löschen wollen?");
     if (deleteBox == true) {
+        // Nächstgelegenen Eintrag mit der Klasse 'entry' finden und entfernen
         let entry = button.closest('.entry');
         entry.remove();
+        // Fortschrittsanzeige aktualisieren (erledigte Todos in Prozent)
         updateErledigtProzent();   
     }
 }
 
+// Funktion zur Berechnung der Priorität basierend auf den Parametern wichtig und dringend
 function berechnePrioritaet(wichtig, dringend) {
     if (wichtig && dringend) {
         return 'schnell erledigen';
@@ -45,11 +62,15 @@ function berechnePrioritaet(wichtig, dringend) {
     }
 }
 
+// Funktion zum Öffnen des Formulars zum Bearbeiten eines bestehenden Eintrags
 function openBearbeiten(button) {
+    // Den Eintrag, der bearbeitet werden soll, finden (nächstgelegener Eintrag)
     zuBearbeitenderEintrag = button.closest('.entry');
 
-    // Textinhalte der Felder ermitteln
+    // Textinhalte der Felder ermitteln und im Formular eintragen
     const titel = zuBearbeitenderEintrag.querySelector('.boxtitel').textContent;
+    // Der Text vom jeweiligen Element wird genommen und beim Doppelpunkt geteilt und nur das zweite Element übernehmen
+    // z.B. Beschreibung: Dies ist eine Beschreibung, dann wird nur "Dies ist eine Beschreibung" im Feld angezeigt
     const beschreibung = zuBearbeitenderEintrag.querySelector('.beschreibungEntry').textContent.split(": ")[1];
     const enddatum = zuBearbeitenderEintrag.querySelector('.enddatumEntry').textContent.split(": ")[1];
     const autor = zuBearbeitenderEintrag.querySelector('.details p:nth-child(1)').textContent.split(": ")[1];
@@ -71,14 +92,19 @@ function openBearbeiten(button) {
     document.getElementById('startdatum').value = startdatum;
     document.getElementById('enddatum').value = enddatum;
 
+    // Priorität im Formular aktualisieren
     updatePrioritaet();
 }
 
+// Funktion zum Hinzufügen eines neuen Eintrags oder Aktualisieren eines bestehenden Eintrags
 function addDaten(titel, beschreibung, autor, kategorie, wichtig, dringend, startdatum, enddatum) {
+    // Container für die Einträge holen
     let container = document.getElementById('eintraege');
+    // Neuen Eintrag erstellen
     let entry = document.createElement('div');
     entry.className = 'entry';
 
+    // Priorität berechnen
     let prioritaet = berechnePrioritaet(wichtig, dringend);
 
     // Symbole entsprechend der Auswahl hinzufügen
@@ -91,6 +117,7 @@ function addDaten(titel, beschreibung, autor, kategorie, wichtig, dringend, star
         symbols = '<b>!</b>'; 
     }
 
+    // HTML-Inhalt des neuen Eintrags
     entry.innerHTML = `
         <div class="header">
             <input type="checkbox" name="abgehackt" class="abgehackt" onclick="updateErledigtProzent(); updateEintragFarbe(this);"><b class="boxtitel">${titel}</b><aside class="symbols">${symbols}</aside>
@@ -110,16 +137,20 @@ function addDaten(titel, beschreibung, autor, kategorie, wichtig, dringend, star
         </div>
     `;
 
+    // Eintrag zum Container hinzufügen
     container.appendChild(entry);
+    // Fortschrittsanzeige aktualisieren
     updateErledigtProzent();
 }
 
+// Funktion zum Umschalten der Anzeige von Details eines Eintrags
 function toggleDetails(button) {
     let entry = button.closest('.entry');
     entry.classList.toggle('expanded');
     button.querySelector('b').classList.toggle('rotate');
 }
 
+// Funktion zur Aktualisierung der Priorität im Formular
 function updatePrioritaet() {
     const wichtig = document.getElementById('wichtig').checked;
     const dringend = document.getElementById('dringend').checked;
@@ -129,6 +160,7 @@ function updatePrioritaet() {
     priorityElement.textContent = `Priorität: ${prioritaet}`;
 }
 
+// Funktion zur Aktualisierung des Fortschritts der erledigten Einträge
 function updateErledigtProzent() {
     const checkboxes = document.querySelectorAll('.abgehackt');
     const total = checkboxes.length;
@@ -137,6 +169,7 @@ function updateErledigtProzent() {
     document.getElementById('anzeigenErledigt').textContent = `TODO's abgehakt: ${percent}%`;
 }
 
+// Funktion zur Aktualisierung der Farbe eines Eintrags basierend auf dem Zustand des Kontrollkästchens
 function updateEintragFarbe(checkbox) {
     let entry = checkbox.closest('.entry');
     let buttons = entry.querySelectorAll('button');  // Selektiert alle Buttons im Eintrag
@@ -155,9 +188,11 @@ function updateEintragFarbe(checkbox) {
             symbols.style.display = '';  // Zeigt die Symbole wieder an
         }
     }
+    // Einträge neu sortieren
     sortiereEintraege();
 }
 
+// Funktion zum Sortieren der Einträge, um erledigte Einträge nach unten zu verschieben
 function sortiereEintraege() {
     let container = document.getElementById('eintraege');
     let entries = Array.from(container.getElementsByClassName('entry'));
@@ -169,9 +204,11 @@ function sortiereEintraege() {
     entries.forEach(entry => container.appendChild(entry));
 }
 
+// Funktion zur Validierung der Formulareingaben
 function validieren(titel, beschreibung, autor, kategorie, startdatum, enddatum){
     let gut = true;
 
+    // Validierung des Titels
     if (titel.length === 0) {
         document.getElementById('errorTitel').textContent = "Titel wird benötigt.";
         document.getElementById('errorTitel').style.display = 'inline';
@@ -184,12 +221,14 @@ function validieren(titel, beschreibung, autor, kategorie, startdatum, enddatum)
         gut = false;
     }
 
+    // Validierung der Beschreibung
     if (beschreibung.length === 0) {
         document.getElementById('errorBeschreibung').textContent = "Beschreibung wird benötigt.";
         document.getElementById('errorBeschreibung').style.display = 'inline';
         gut = false;
     }
 
+    // Validierung des Autors
     if (autor.length === 0) {
         document.getElementById('errorAutor').textContent = "Autor wird benötigt.";
         document.getElementById('errorAutor').style.display = 'inline';
@@ -202,18 +241,21 @@ function validieren(titel, beschreibung, autor, kategorie, startdatum, enddatum)
         gut = false;
     }
 
+    // Validierung der Kategorie
     if (kategorie === "default") {
         document.getElementById('errorKategorie').textContent = "Kategorie wird benötigt.";
         document.getElementById('errorKategorie').style.display = 'inline';
         gut = false;
     }
 
+    // Validierung des Startdatums
     if (startdatum.length === 0) {
         document.getElementById('errorStartdatum').textContent = "Startdatum wird benötigt.";
         document.getElementById('errorStartdatum').style.display = 'inline';
         gut = false;
     }
 
+    // Validierung des Enddatums
     if (enddatum.length === 0) {
         document.getElementById('errorEnddatum').textContent = "Enddatum wird benötigt.";
         document.getElementById('errorEnddatum').style.display = 'inline';
@@ -223,12 +265,16 @@ function validieren(titel, beschreibung, autor, kategorie, startdatum, enddatum)
     return gut;
 }
 
+// Event-Listener für DOMContentLoaded, um das Formular und andere Elemente zu initialisieren
 document.addEventListener('DOMContentLoaded', function() {
+    // Event-Listener für das Formular zum Hinzufügen/Bearbeiten eines Eintrags
     document.getElementById('formHinzufuegen').addEventListener('submit', function(e) {
         e.preventDefault();
 
+        // Alle Fehlermeldungen ausblenden
         document.querySelectorAll('.error').forEach(error => error.style.display = 'none');
 
+        // Werte aus dem Formular holen
         let titel = document.getElementById('titel').value;
         let beschreibung = document.getElementById('beschreibung').value;
         let autor = document.getElementById('autor').value;
@@ -238,7 +284,9 @@ document.addEventListener('DOMContentLoaded', function() {
         let startdatum = document.getElementById('startdatum').value;
         let enddatum = document.getElementById('enddatum').value;
 
+        // Validierung der Eingaben
         if (validieren(titel, beschreibung, autor, kategorie, startdatum, enddatum)) {
+            // Wenn ein Eintrag bearbeitet wird, diesen aktualisieren
             if (zuBearbeitenderEintrag) {
                 zuBearbeitenderEintrag.querySelector('.boxtitel').textContent = titel;
                 zuBearbeitenderEintrag.querySelector('.beschreibungEntry').textContent = "Beschreibung: " + beschreibung;
@@ -250,10 +298,12 @@ document.addEventListener('DOMContentLoaded', function() {
                 zuBearbeitenderEintrag.querySelector('.details p:nth-child(5)').textContent = "Startdatum: " + startdatum;
                 zuBearbeitenderEintrag.querySelector('.details p:nth-child(6)').textContent = "Priorität: " + berechnePrioritaet(wichtig, dringend);
 
+                // Formular und Bearbeitungsstatus zurücksetzen
                 zuBearbeitenderEintrag = null;
                 document.getElementById('formHinzufuegen').reset();
                 closeHinzufuegen();
             } else {
+                // Andernfalls neuen Eintrag hinzufügen
                 addDaten(titel, beschreibung, autor, kategorie, wichtig, dringend, startdatum, enddatum);
                 document.getElementById('formHinzufuegen').reset();
                 closeHinzufuegen();
@@ -261,7 +311,9 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
+    // Event-Listener für Änderungen der Wichtigkeit und Dringlichkeit
     document.getElementById('wichtig').addEventListener('change', updatePrioritaet);
     document.getElementById('dringend').addEventListener('change', updatePrioritaet);
+    // Event-Listener für die Eingabe in der Suchleiste
     document.getElementById('searchbar').addEventListener('input', searchResultat);
 });
